82391068efca6f3ae707c64a1627c342874d01f4
MijdeMijs <midasstorm@gmail.com>
2024-12-23 23:50:03 +0000

    Basic layout and requirements

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,13 @@
 import streamlit as st
+from imdb import Cinemagoer
 
-st.title("🎈 My new app")
+st.title("Thursday Filmday :clapper::film_projector:")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+st.header("Film Chooser", divider="rainbow")
+
+st.header("Movie Stats", divider="rainbow")
+
+st.header("Archive", divider="rainbow")
